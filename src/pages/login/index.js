381ec07bacbef309566f50cb7c7142532888f978
VitorKreis <101025640+VitorKreis@users.noreply.github.com>
2023-03-02 17:58:10 -0300

    Instalando axios e aprendendo a utilizar

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { toast } from "react-toastify";
 import { Title, Paragraf } from "./styled";
 import { Container } from "../../styles/GlobalStyles";
+import axios from "../../services/axios";
 
 export default function Login() {
+  /*
+  useEffect(() => {
+    async function getAlunos() {
+      const res = await axios.get("/alunos");
+      const { data } = res;
+
+      console.log(data);
+    }
+    getAlunos();
+  }, []);
+  Aprendendo a utilizar useEffect e axios */
   return (
     <Container>
       <Title hasName>Ola</Title>
-
       <Paragraf>Qualquer coisa do login</Paragraf>
     </Container>
   );
